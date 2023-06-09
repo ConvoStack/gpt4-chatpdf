5181b1bdc3819d99e6c6a336221e910e0af464ab
radami <imanwiz1@gmail.com>
2023-06-09 16:12:13 -0400

    chatpdf

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
-# ConvoStack QuickStart Guide
+# ChatPDF with ConvoStack
 
 ![dev-playground](https://github.com/ConvoStack/quickstart/assets/8688852/940e166e-28a7-44a7-8d79-879598ccdc9b)
 
-Welcome to the ConvoStack Quickstart walkthrough! To follow along in our docs: https://docs.convostack.ai/getting-started/
+Inspired by mayooear's [gpt4-pdf-chatbot-langchain](https://github.com/mayooear/gpt4-pdf-chatbot-langchain) repository, we have integrated ConvoStack to create a chatGPT chatbot playground for multiple Large PDF files.
 
-In this repo's `index.ts` file, you will find three examples of connecting a Langchain OpenAI agent to the ConvoStack chatbot playground.
+[Join the discord if you have questions](https://discord.com/invite/gCGbAm9HXx)
 
-## Getting Started
+## Development
 
-Run `npm install` to install all necessary dependencies.
+1. Run `npm install` to install all necessary dependencies.
 
-To run an example agent, simply set your `OPENAI_API_KEY` in a `.env` file and run `npx ts-node src/index.ts` to generate a url to the ConvoStack chatbot playground.
+2. Set up your `.env` file
+
+- Copy `.env.example` into `.env`
+  Your `.env` file should look like this:
+
+```
+OPENAI_API_KEY=
+
+PINECONE_API_KEY=
+PINECONE_ENVIRONMENT=
+
+PINECONE_INDEX_NAME=
+
+```
+
+- Visit [openai](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key) to retrieve API keys and insert into your `.env` file.
+- Visit [pinecone](https://pinecone.io/) to create and retrieve your API keys, and also retrieve your environment and index name from the dashboard.
+
+3. In the `config` folder, replace the `PINECONE_NAME_SPACE` with a `namespace` where you'd like to store your embeddings on Pinecone when you run `npm run ingest`. This namespace will later be used for queries and retrieval.
+
+## Convert your PDF files to embeddings
+
+**This repo can load multiple PDF files**
+
+1. Create a `docs` folder. Inside `docs` folder, add your pdf files or folders that contain pdf files.
+
+2. Run the script `npm run ingest` to 'ingest' and embed your docs. If you run into errors troubleshoot below.
+
+3. Check Pinecone dashboard to verify your namespace and vectors have been added.
+
+4. In `src/index.ts`, change the `templates.qaPrompt` for your own usecase. Change `modelName` in `new OpenAI` to `gpt-4`, if you have access to `gpt-4` api. Please verify outside this repo that you have access to `gpt-4` api, otherwise the application will not work.
+
+## Run the app
+
+Once you've verified that the embeddings and content have been successfully added to your Pinecone, you can run the app `npm run dev` to launch the ConvoStack chatbot playground.
 
 ## Suport ConvoStack
 
--- src/index.ts
+++ src/index.ts
@@ -3,136 +3,71 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { playground } from "convostack/playground";
-import {
-  IAgentContext,
-  IAgentResponse,
-  IAgentCallbacks,
-} from "convostack/agent";
-import { ConvoStackLangchainChatMessageHistory } from "convostack/langchain-memory";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { OpenAI } from "langchain/llms/openai";
-import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  MessagesPlaceholder,
-  SystemMessagePromptTemplate,
-} from "langchain/prompts";
-import { ConversationChain, LLMChain } from "langchain/chains";
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { BufferMemory } from "langchain/memory";
+import { IAgentContext, IAgentResponse } from "convostack/agent";
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from "../config/pinecone";
+import { templates } from "./templates";
 
-// Welcome to the ConvoStack Quickstart walkthrough! To follow along in our docs: https://docs.convostack.ai/getting-started/
-
-// To support ConvoStack as an open-source AI project, feel free to give our repo a ⭐ : https://github.com/ConvoStack/convostack
-
-// Example 1: OpenAI Agent
-// playground({
-//   async reply(context: IAgentContext): Promise<IAgentResponse> {
-//     // `humanMessage` is the content of each message the user sends via the chatbot playground.
-//     let humanMessage = context.getHumanMessage().content;
-//     // `agent` is the OpenAI agent we want to use to respond to each `humanMessage`
-//     const agent = new OpenAI({ modelName: "gpt-3.5-turbo" });
-//     // `call` is a simple string-in, string-out method for interacting with the OpenAI agent.
-//     const resp = await agent.call(humanMessage);
-//     // `resp` is the generated agent's response to the user's `humanMessage`
-//     return {
-//       content: resp,
-//       contentType: "markdown",
-//     };
-//   },
-// });
-
-// Example 2: LLM Chain
-// playground({
-//   async reply(context: IAgentContext): Promise<IAgentResponse> {
-//     // `humanMessage` is the content of each message the user sends via the chatbot playground.
-//     let humanMessage = context.getHumanMessage().content;
-//     // We can now construct an LLMChain from a ChatPromptTemplate and a chat model.
-//     const chat = new ChatOpenAI({ streaming: true, temperature: 0 });
-//     // Pre-prompt the agent to be a language translator
-//     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-//       SystemMessagePromptTemplate.fromTemplate(
-//         "You are a helpful assistant that translates {input_language} to {output_language}."
-//       ),
-//       HumanMessagePromptTemplate.fromTemplate("{text}"),
-//     ]);
-//     const chain = new LLMChain({
-//       prompt: chatPrompt,
-//       llm: chat,
-//     });
-
-//     // `resp` is the response of the OpenAI LLM chain translating `humanMessage` from English to French.
-//     const resp = await chain.call({
-//       input_language: "English",
-//       output_language: "French",
-//       text: humanMessage,
-//     });
-
-//     return {
-//       content: resp.text,
-//       contentType: "markdown",
-//     };
-//   },
-// });
-
-// Example 3: LLM Chain With History
 playground({
-  async reply(
-    context: IAgentContext,
-    callbacks?: IAgentCallbacks
-  ): Promise<IAgentResponse> {
-    // `humanMessage` is the content of each message the user sends via the chatbot playground.
-    let humanMessage = context.getHumanMessage().content;
+  async reply(context: IAgentContext): Promise<IAgentResponse> {
+    const pinecone = new PineconeClient();
+    await pinecone.init({
+      environment: process.env.PINECONE_ENVIRONMENT!,
+      apiKey: process.env.PINECONE_API_KEY!,
+    });
+    const llm = new OpenAI({});
+    const question = context.getHumanMessage().content;
+    try {
+      // Build an LLM chain that will improve the user prompt
+      const index = pinecone.Index(PINECONE_INDEX_NAME);
 
-    // Create a new OpenAI agent, with streaming
-    const chat = new ChatOpenAI({
-      modelName: "gpt-3.5-turbo",
-      temperature: 0,
-      streaming: true,
-      callbacks: [
+      /* create vectorstore*/
+      const vectorStore = await PineconeStore.fromExistingIndex(
+        new OpenAIEmbeddings({}),
         {
-          handleLLMNewToken(token: string) {
-            // Stream tokens to ConvoStack
-            callbacks?.onMessagePart({
-              contentChunk: token,
-            });
-          },
-        },
-      ],
-    });
+          pineconeIndex: index,
+          textKey: "text",
+          namespace: PINECONE_NAME_SPACE, //namespace comes from your config folder
+        }
+      );
 
-    // Setup your prompts (note the placeholder for {history})
-    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-      SystemMessagePromptTemplate.fromTemplate(
-        "The following is a friendly conversation between a human and an AI."
-      ),
-      new MessagesPlaceholder("history"),
-      HumanMessagePromptTemplate.fromTemplate("{input}"),
-    ]);
+      //create chain
+      const model = new OpenAI({
+        temperature: 0, // increase temepreature to get more creative answers
+        modelName: "gpt-3.5-turbo", //change this to gpt-4 if you have access
+      });
 
-    // Setup the chain with a BufferMemory that pulls from the ConvoStack conversation history
-    const chain = new ConversationChain({
-      memory: new BufferMemory({
-        // Use the ConvoStackLangchainChatMessageHistory class to prepare a Langchain-compatible version of the history
-        chatHistory: new ConvoStackLangchainChatMessageHistory({
-          // Pass the current conversation's message history for loading
-          history: context.getHistory(),
-        }),
-        returnMessages: true,
-        memoryKey: "history",
-      }),
-      prompt: chatPrompt,
-      llm: chat,
-    });
+      const chain = ConversationalRetrievalQAChain.fromLLM(
+        model,
+        vectorStore.asRetriever(),
+        {
+          qaTemplate: templates.qaPrompt,
+          questionGeneratorTemplate: templates.condensePrompt,
+          returnSourceDocuments: true, //The number of source documents returned is 4 by default
+        }
+      );
+      const history = context.getHistory().map((item) => item.content);
 
-    // `resp` is the response of the OpenAI LLM chain to `humanMessage`, which was inputted on the ConvoStack playground.
-    const resp = await chain.call({
-      input: context.getHumanMessage().content,
-    });
+      //Ask a question using chat history
+      const response = await chain.call({
+        question: question,
+        chat_history: history,
+      });
 
-    // Send the final response to ConvoStack
-    return {
-      content: resp.response,
-      contentType: "markdown",
-    };
+      return {
+        content: response.text,
+        contentType: "markdown",
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        content: "error",
+        contentType: "markdown",
+      };
+    }
   },
 });
